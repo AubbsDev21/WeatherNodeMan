7201770ff38c847c86ab462e03604cb675ce762f
Aubre <bodyaubre@gmail.com>
2019-01-13 16:17:34 -0500

    transfer from old to new

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-'use strict';
+const express = require('express')
+//instant server restart
+const request = require('request')
+var app = express();
 
-var fs = require('fs');
-var path = require('path');
+app.set('view engine', 'ejs')
 
-exports.get = function(event, context, callback) {
-  var contents = fs.readFileSync(`public${path.sep}index.html`);
-  var result = {
-    statusCode: 200,
-    body: contents.toString(),
-    headers: {'content-type': 'text/html'}
-  };
+var city = 'Charlotte';
+var URl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1`;
 
-  callback(null, result);
-};
+app.get('/', function(req, res) {
+    request(URl, function(error, response, body) {
+         weather_json = JSON.parse(body);
+
+         console.log(weather_json);
+        
+      // creating the data keys
+         var weather = {
+             city: city ,
+            temperature: Math.round(weather_json.main.temp),
+            description: weather_json.weather[0].description,
+            icon: weather_json.weather[0].icon
+         };
+
+         var weather_data = {
+             weather : weather
+         };
+            res.render('index', weather_data );
+    });
+    
+
+});
+
+
+app.listen(8000);
